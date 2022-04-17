f0518ed5c0ace178373d8ea408a9d55daf793f2b
pjs0418 <tony180322@gmail.com>
2022-04-17 14:20:26 +0900

    feat: Implement function for Timer

--- Pomodoro/ViewController.swift
+++ Pomodoro/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import AudioToolbox
 
 enum TimerStatus {
     case start
@@ -22,6 +23,8 @@ class ViewController: UIViewController {
     
     var duration = 60
     var timerStatus: TimerStatus = .end
+    var timer: DispatchSourceTimer?
+    var currentSeconds = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -38,14 +41,47 @@ class ViewController: UIViewController {
         self.toggleButton.setTitle("일시정지", for: .selected)
     }
     
+    func startTimer() {
+        if self.timer == nil {
+            self.timer = DispatchSource.makeTimerSource(flags: [], queue: .main)
+            self.timer?.schedule(deadline: .now(), repeating: 1)
+            self.timer?.setEventHandler(handler: { [weak self] in
+                guard let self = self else { return }
+                self.currentSeconds -= 1
+                let hour = self.currentSeconds / 3600
+                let minutes = (self.currentSeconds % 3600) / 60
+                let seconds = (self.currentSeconds % 3600) % 60
+                self.timerLabel.text = String(format: "%02d:%02d:%02d", hour, minutes, seconds)
+                self.progressView.progress = Float(self.currentSeconds) / Float(self.duration)
+                
+                if self.currentSeconds <= 0 {
+                    self.stopTimer()
+                    AudioServicesPlaySystemSound(1005)
+                }
+            })
+            self.timer?.resume()
+        }
+    }
+    
+    func stopTimer() {
+        if self.timerStatus == .pause {
+            self.timer?.resume()
+        }
+        
+        self.timerStatus = .end
+        self.cancelButton.isEnabled = false
+        self.setTimerInfoViewVisible(isHidden: true)
+        self.datePicker.isHidden = false
+        self.toggleButton.isSelected = false
+        self.timer?.cancel()
+        self.timer = nil
+    }
+    
     @IBAction func tapCancelButton(_ sender: UIButton) {
         switch self.timerStatus {
         case .start, .pause:
-            self.timerStatus = .end
-            self.cancelButton.isEnabled = false
-            self.setTimerInfoViewVisible(isHidden: true)
-            self.datePicker.isHidden = false
-            self.toggleButton.isSelected = false
+            self.stopTimer()
+            
         default:
             break
         }
@@ -56,17 +92,23 @@ class ViewController: UIViewController {
         
         switch self.timerStatus {
         case .end:
+            self.currentSeconds = self.duration
             self.timerStatus = .start
             self.setTimerInfoViewVisible(isHidden: false)
             self.datePicker.isHidden = true
             self.toggleButton.isSelected = true
             self.cancelButton.isEnabled = true
+            self.startTimer()
+            
         case .start:
             self.timerStatus = .pause
             self.toggleButton.isSelected = false
+            self.timer?.suspend()
+            
         case .pause:
             self.timerStatus = .start
             self.toggleButton.isSelected = true
+            self.timer?.resume()
         }
     }
 }
